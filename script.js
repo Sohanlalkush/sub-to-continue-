e3d925d21a95824c45a9fe27ab983422773b1db9
Sohanlalkush <100574470+Sohanlalkush@users.noreply.github.com>
2023-01-09 02:22:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,68 @@
+
+  function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let Follow = getCookie("Followed");
+  if (Follow != "") {loloj()
+  } else{lolo()
+  }
+}
+    window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {checkCookie()
+  }
+}
+    
+    
+    function loloe(){
+document.getElementById("cexit").style.visibility = "visible";
+document.getElementById("cexit").style.opacity = "2";
+}
+function lolo(){
+    document.getElementById("popup1").style.visibility  = "visible";
+    document.getElementById("popup1").style.opacity  = "1";
+ document.getElementById("cexit")    .style.visibility = "hide";
+document.getElementById("cexit").style.opacity = "0";
+}
+
+function confirm(){
+  document.cookie = "followed=yes; expires=Mon, 01 Jan 2024 12:00:00 UTC";
+  window.location.reload();
+}
+
+function followfb(){ 
+window.open('https://Facebook.com/Pharmalite.in','_blank') ;
+confirm()
+    
+}
+
+function followyt(){ 
+window.open('https://youtube.com/@pharmalite','_blank') ;
+confirm()
+    
+}
+
+function followinsta(){ 
+window.open('https://instagram.com/Pharmalite.in','_blank') ;
+confirm()
+    
+}
+
+function loloj(){
+    document.getElementById("popup1").style.visibility  = "invisible";
+ document.getElementById("popup1").style.opacity  = "0";
+ }
